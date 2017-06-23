c888026e6d740ab5fb3803a32c07061f12332b9c
Alan Leung <alanlcode@gmail.com>
2017-06-23 15:28:41 -0700

    Move codox to :dev profile.  Configure github source links.

--- project.clj
+++ project.clj
@@ -8,7 +8,9 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [clojail "1.0.6"]
                  [clj-time "0.11.0"]]
-  :plugins  [[codox "0.9.1"]]
   :profiles {:dev {:dependencies [[expectations "2.0.9"]]
                    :plugins [[lein-expectations "0.0.8"]
-                             [lein-autoexpect "1.7.0"]]}})
+                             [lein-autoexpect "1.7.0"]
+                             [codox "0.8.10"]]
+                   :codox {:src-dir-uri "https://github.com/alanlcode/util/blob/master/"
+                           :src-linenum-anchor-prefix "L"}}})
